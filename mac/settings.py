addcc4185472f8c097992983d57b7671a62b3a86
Aakash Kumar Jha <aakash13102005u@gmail.com>
2023-10-27 07:10:10 +0530

    changes

--- mac/settings.py
+++ mac/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = 'django-insecure-+4rgk3(98im#b=cne7s#k$2@f1ha*c_wpi1crnpxae13kmec8(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'django-ecommerce-website-by-aakashmid.onrender.com',
-    'djangoEcommerce.aakashmid.app',
+ALLOWED_HOSTS = ['django-ecommerce-website-by-aakashmid.onrender.com',
+
     # Add other allowed hosts if necessary
 ]
 # ALLOWED_HOSTS = ["127.0.0.1","django-ecommerce-w?ebsite-by-aakashmid.onrender.com"]
